a4862e938aff832bc6339da5950074fd573fe803
cs2jang <cs2jang@gmail.com>
2020-08-17 23:33:12 +0900

    added getMenu

--- app.py
+++ app.py
@@ -9,9 +9,27 @@ def userLogin():
     print(jsonify(user))
     return jsonify(user)
  
-@app.route('/environments/<language>')
-def environments(language):
-    return jsonify({"language":language})
+@app.route('/getMenu', methods = ['POST'])
+def getMenu(language):
+    req_dict = request.get_json()
+    user_said = req_dict['userRequest']['utterance']
+    user_detail = req_dict['action']['detailParams']
+    user_req_date = user_detail['sys_date']
+    user_req_menu = user_detail['kart_menu']
+
+    print(user_said, user_req_date, user_req_menu)
+    res_dict = dict()
+    res_dict['version'] = "1.0"
+    res_dict['template'] = {
+        'output' : [
+            {
+                'simpleText' : {
+                    'text' : "오늘의 메뉴는 맛있는 밥입니다아!"
+                }
+            }
+        ]
+    }
+    return jsonify(res_dict)
 
 @app.route('/')
 def hello_world():
