6396d29d688f1bd83ad50f3ee483fc06329ec985
cs2jang <cs2jang@gmail.com>
2021-10-12 00:30:22 +0900

    update dict

--- gdata.py
+++ gdata.py
@@ -7,16 +7,16 @@ from oauth2client.service_account import ServiceAccountCredentials
 class GS:
     def __init__(self):
         keydict = {
-            "type" : environ.get('type'),
-            "project_id" : environ.get('project_id'),
-            "private_key_id" : environ.get('private_key_id'),
-            "private_key" : environ.get('private_key'),
-            "client_email" : environ.get('client_email'),
-            "client_id" : environ.get('client_id'),
-            "auth_uri" : environ.get('auth_uri'),
-            "token_uri" : environ.get('token_uri'),
-            "auth_provider_x509_cert_url" : environ.get('auth_provider_x509_cert_url'),
-            "client_x509_cert_url" : environ.get('client_x509_cert_url'),
+            "type" : environ.get("type"),
+            "project_id" : environ.get("project_id"),
+            "private_key_id" : environ.get("private_key_id"),
+            "private_key" : environ.get("private_key"),
+            "client_email" : environ.get("client_email"),
+            "client_id" : environ.get("client_id"),
+            "auth_uri" : environ.get("auth_uri"),
+            "token_uri" : environ.get("token_uri"),
+            "auth_provider_x509_cert_url" : environ.get("auth_provider_x509_cert_url"),
+            "client_x509_cert_url" : environ.get("client_x509_cert_url"),
         }
         scope = ['https://spreadsheets.google.com/feeds']
         print(keydict)
